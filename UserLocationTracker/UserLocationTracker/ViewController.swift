2ccca0a15d8e1bf787bf4611b99934c06fa2ca49
Antonio de Mingo <tonidemingo@gmail.com>
2019-02-15 21:40:30 +0100

    Project init

---
+++ UserLocationTracker/UserLocationTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserLocationTracker
+//
+//  Created by Antonio De Mingo Navarro on 15/02/2019.
+//  Copyright © 2019 Antonio De Mingo Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

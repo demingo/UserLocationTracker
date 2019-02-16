8dde1bf37f3a23eb2468041c7a204c8b616c41f0
Antonio de Mingo <tonidemingo@gmail.com>
2019-02-16 11:43:45 +0100

    locationManager

--- UserLocationTracker/UserLocationTracker/ViewController.swift
+++ UserLocationTracker/UserLocationTracker/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func checkLocationServices() { // Comprueba que Location está activado en el teléfono
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+        } else {
+            // Dar indicación al usuario
+        }
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+    }
+}
